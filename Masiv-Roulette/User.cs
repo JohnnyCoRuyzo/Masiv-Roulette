c8818186b85e077054f20e1a0b804d41a58bf3b8
Johnnatan Ruiz <johnnyruyzo@gmail.com>
2021-01-17 14:21:58 -0500

    Change: Creation of class User.

---
+++ Masiv-Roulette/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Masiv_Roulette
+{
+    public class User
+    {
+        public Guid ID { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public User(string userName)
+        {
+            ID = Guid.NewGuid();
+            UserName = userName;
+            Password = GeneratePasswordAndSendEmail();
+            Balance = 0;
+        }
+
+        public string GeneratePasswordAndSendEmail()
+        {
+            var password = GeneratePasswordString();
+            SendEmail(password);
+            return password;
+        }
+
+        public string GeneratePasswordString()
+        {
+            return "password";
+        }
+
+        public void SendEmail(string password)
+        {
+            string _ = "Your credentials are: " + password;
+        }
+    }
+}
