7d2b41fc30a4afacdc231f957059cc808c286360
Johnnatan Ruiz <johnnyruyzo@gmail.com>
2021-01-17 13:16:37 -0500

    Agregar archivos de proyecto.

---
+++ Masiv-Roulette/Controllers/RouletteController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Masiv_Roulette.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RouletteController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<RouletteController> _logger;
+
+        public RouletteController(ILogger<RouletteController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Roulette> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Roulette
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ Masiv-Roulette/Roulette.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace Masiv_Roulette
+{
+    public class Roulette
+    {
+        public Guid ID { get; set; }
+
+        public bool IsRouletteOpen { get; set; }
+
+        public int ResultNumber { get; set; }
+
+        public string ResultColor { get; set; }
+
+        public const int MIN_ROULETTE_RESULT = 0;
+
+        public const int MAX_ROULETTE_RESULT = 36;
+
+        public const string RED_COLOR = "Rojo";
+
+        public const string BLACK_COLOR = "Negro";
+
+        public const string GREEN_COLOR = "Verde";
+
+        public void SpinRoulette()
+        {
+            var randomRange = new Random();
+            ResultNumber = randomRange.Next(MIN_ROULETTE_RESULT, MAX_ROULETTE_RESULT);
+            GetResultingColorFromSpin();
+        }
+
+        public void GetResultingColorFromSpin()
+        {
+            if (ResultNumber == 0)
+                ResultColor = GREEN_COLOR;
+            else if (ResultNumber % 2 == 0)
+                ResultColor = RED_COLOR;
+            else
+                ResultColor = BLACK_COLOR;
+        }
+    }
+}
