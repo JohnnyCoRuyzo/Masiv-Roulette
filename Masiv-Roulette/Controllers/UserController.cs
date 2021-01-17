121113dabe2c8d89c818e352e46c6a27e5f79f79
Johnnatan Ruiz <johnnyruyzo@gmail.com>
2021-01-17 14:24:58 -0500

    Change: Creation of the CreateUser API.

---
+++ Masiv-Roulette/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Masiv_Roulette.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("/CreateUser/{userName}")]
+        public Guid CreateUser(string userName)
+        {
+            User userToCreate = new User(userName);
+            return userToCreate.ID;
+        }
+    }
+}
