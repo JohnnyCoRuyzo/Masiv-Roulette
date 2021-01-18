d1b1c156c8e48cfc10930ffec979610292baad2c
Johnnatan Ruiz <johnnyruyzo@gmail.com>
2021-01-18 00:27:50 -0500

    Change: Removing unnecesary usings.

--- Masiv-Roulette/Controllers/RouletteController.cs
+++ Masiv-Roulette/Controllers/RouletteController.cs
@@ -1,5 +1,4 @@
-﻿using Masiv_Roulette.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
